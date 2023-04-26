d3d5e62997116ac3d5eb992eb1cda67905a1a29e
Ivo Brandao <ivo.couceiro.brandao@gmail.com>
2023-04-27 00:29:41 +0200

    updated dependencies

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,3 +1,14 @@
+sudo snap install nvim --classic
 sudo apt install xsel -y
-pip3 install pynvim
-npm i -g neovim
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+sudo apt install npm -y
+sudo apt install nodejs -y
+sudo npm i -g neovim
+sudo apt install ripgrep -y
+sudo apt install fzf -y
+sudo apt install fd-find -y
+sudo apt install universal-ctags -y
+sudo apt install silversearcher-ag -y
+
+
